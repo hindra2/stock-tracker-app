062b299870c12566f72ff4c39d405914c2ad22b8
H1R0L9 <53500251+H1R0L9@users.noreply.github.com>
2023-02-09 23:28:37 +0700

    sync

--- wishlistPage.py
+++ wishlistPage.py
@@ -31,23 +31,23 @@ class wishlistPage(ctk.CTkFrame):
                 content = dictionary[ticker]
                 self.card = ctk.CTkFrame(self.display_frame, corner_radius=21)
                 self.card.grid(row=row, padx=5, pady=5, sticky="we")
-                # self.card.rowconfigure((0,1), weight=1)
-                # self.card.columnconfigure((0, 1, 2), weight=1)
+                self.card.rowconfigure((0,1), weight=1)
+                self.card.columnconfigure((0, 1, 2), weight=1)
 
                 self.ticker_label = ctk.CTkLabel(self.card, text=ticker, font=("Arial", 15))
-                self.ticker_label.grid(row=0, column=0, padx=5, pady=5, sticky="we")
+                self.ticker_label.grid(row=0, column=0, padx=5, pady=5, sticky="w")
 
                 self.count_label = ctk.CTkLabel(self.card, text=content["Count"], font=("Arial", 15))
-                self.count_label.grid(row=1, column=0, padx=5, pady=5, sticky="we")
+                self.count_label.grid(row=1, column=0, padx=5, pady=5, sticky="w")
 
                 self.boughtprice_label = ctk.CTkLabel(self.card, text=content["Bought Price"], font=("Arial", 15))
-                self.boughtprice_label.grid(row=0, column=1, padx=5, pady=5, sticky="we")
+                self.boughtprice_label.grid(row=0, column=1, padx=5, pady=5, sticky="w")
 
                 self.upperbound_label = ctk.CTkLabel(self.card, text=content["Upper Bound"], font=("Arial", 15))
-                self.upperbound_label.grid(row=0, column=2, padx=5, pady=5, sticky="we")
+                self.upperbound_label.grid(row=0, column=2, padx=5, pady=5, sticky="e")
 
                 self.lowerbound_label = ctk.CTkLabel(self.card, text=content["Lower Bound"], font=("Arial", 15))
-                self.lowerbound_label.grid(row=1, column=2, padx=5, pady=5, sticky="we")
+                self.lowerbound_label.grid(row=1, column=2, padx=5, pady=5, sticky="e")
 
             row += 1
 
